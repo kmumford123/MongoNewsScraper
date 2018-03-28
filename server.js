2992712deeacb450304de2f0f4c21982fc70e915
Khamel <khamel.mumford@gmail.com>
2018-03-27 23:11:21 -0500

    server.js construct and heroku tasks

---
+++ server.js
@@ -0,0 +1,22 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+var PORT = process.env.PORT || 3000;
+var exphbs = require("express-handlebars");
+
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+// var routes = require("./controllers/burgersController");
+
+// app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+});

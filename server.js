697aaf358ba04d7f7844321ad5b7058a91d5c732
Khamel <khamel.mumford@gmail.com>
2018-03-31 12:39:46 -0500

    better web address for scrapingmongo

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var request = require("request");
 var cheerio = require("cheerio");
 
 app.use(express.static(__dirname + "/public"));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
@@ -33,7 +33,7 @@ app.get("", function(req, res) {
 // Route 1
 // =======
 app.get("/scrape", function(req, res) {
-    request("https://www.cnn.com/us", function(error, response, html) {
+    request("https://www.cnn.com/specials/last-50-stories", function(error, response, html) {
         // Load the body of the HTML into cheerio
         var $ = cheerio.load(html);
         // Empty array to save our scraped data
@@ -45,13 +45,22 @@ app.get("/scrape", function(req, res) {
             // Find the h4 tag's parent a-tag, and save it's href value as "link"
             var link = $(element).children().attr("href");
             // Make an object with data we scraped for this h4 and push it to the results array
+
             results.push({
                 title,
                 link
             });
+            //loop to check for duplicates
+            dbdata = db.scrapedData.find({})
+                // var dbCheck = results.each(function(i, ) {
+
+            // })
             db.scrapedData.insert({ results })
         });
-        res.json(results);
+        var cnnscrape = res.json(results)
+        res.render("index", {
+            user: cnnscrape
+        });
     })
 })
 
